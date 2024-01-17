8e1572d6530262175c15547e548f5a74b6e20c43
gavriel <gbatyrov10@gmail.com>
2024-01-17 15:00:54 -0500

    finnished

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const numOne = 10;
-const numTwo = 40;
-const numThree = 39;
+const code1 = 5 + 5;
+
+// Using multiplication (*) to get 40
+const code2 = 8 * 5;
+
+// Using division (/) to get 39
+const code3 = 78 / 2;
 
 console.log(
   "You have received this message because you have been chosen to open an important vault. Here is the secret combination"
 );
-
-console.log("10-40-39");
+console.log(
+  "The combination for the lock  is " + code1 + " - " + code2 + " - " + code3
+);
