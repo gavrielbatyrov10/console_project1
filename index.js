bd0bb196cef120a5d9ba53536d43f9d4907bc32d
gavriel <gbatyrov10@gmail.com>
2024-01-17 14:48:38 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const numOne = 10;
+const numTwo = 40;
+const numThree = 39;
+
+console.log(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination"
+);
