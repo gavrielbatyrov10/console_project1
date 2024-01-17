d13fa00503ee8150bb817f6ffab0c3006d4a40c5
gavriel <gbatyrov10@gmail.com>
2024-01-17 14:50:55 -0500

    added the code

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const numThree = 39;
 console.log(
   "You have received this message because you have been chosen to open an important vault. Here is the secret combination"
 );
+
+console.log("10-40-39");
